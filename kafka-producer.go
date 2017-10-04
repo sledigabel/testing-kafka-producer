cdca39c5933e32f35af62fcc953c56e8c8da6ad0
Sebastien Le Digabel <sledigabel@medallia.com>
2017-10-04 15:54:15 +0100

    Working but inefficient

--- kafka-producer.go
+++ kafka-producer.go
@@ -13,11 +13,16 @@ import (
 var debug bool
 var dryrun bool
 
-var msgChan chan string
+var msgChan chan kafka.Message
+var done chan bool
+var sent chan bool
+
+
+const batchSize = 2048
 
 func PrintDebug(s string) {
 	if debug {
-		fmt.Println("[DEBUG] " + s)
+		fmt.Println("[DEBUG] "+ time.Now().Format(time.RFC3339) + " " + s)
 	}
 }
 
@@ -29,52 +34,79 @@ func usage() {
 
 
 //
-func ProduceMessages(number int){
+func ProduceMessages(number int, timeout time.Duration){
 	ticker := time.NewTicker(time.Second)
+	goodbye := time.NewTimer(timeout)
 
-	for range ticker.C {
-		PrintDebug("BLEEP")
-		for i := 1; i <= number; i++ {
-			u := uuid.New()
-			// TODO: make a better msg using json
-			msgChan <- fmt.Sprintf("%s%s%s",u.String(),u.String(),u.String())
+	for{
+		select {
+			case <- ticker.C:
+				PrintDebug("BLEEP")
+				for i := 1; i <= number; i++ {
+					u := uuid.New()
+					// TODO: make a better msg using json
+					msgChan <- kafka.Message{Value: []byte(fmt.Sprintf("%s%s%s",u.String(),u.String(),u.String()))}
+				}
+			case <- goodbye.C:
+				PrintDebug("Timeout!")
+				PrintDebug("Finished producing")
+				done <- true
+				close(msgChan)
+				return
 		}
 	}
-	msgChan <- "EOF"
-
+	PrintDebug("Finished producing")
+	done <- true
 }
 
 func readMessages(){
 	PrintDebug("Starting reading thread")
 	for i := range msgChan{
-		fmt.Println(i)
-		if i == "EOF" {
-			break
-		}
+		fmt.Println(string(i.Value))
 	}
+	sent <- true
+	PrintDebug("Finished reading thread")
 }
 
 func sendMessages(w *kafka.Writer){
 	// TODO
 	//fmt.Println(producer,kfk_prod,topic)
 	PrintDebug("Starting sending to Kafka")
-	for i := range msgChan{
-		if i == "EOF" {
-			break
+
+	batch := make([]kafka.Message,0)
+
+	for k := range msgChan {
+		if len(batch) < batchSize {
+			batch = append(batch, k)
+		} else {
+			PrintDebug("Sending batch full of data")
+			err := w.WriteMessages(context.Background(),batch...)
+			if err != nil {
+				fmt.Printf("Failed to publish messages: %s\n", err)
+				os.Exit(1)
+			}
+			batch = []kafka.Message{k}
 		}
-		err := w.WriteMessages(context.Background(),kafka.Message{Value: []byte(i)})
+		//PrintDebug(fmt.Sprintf("Batchsize : %d",len(batch)))
+	}
+	PrintDebug("Ending")
+	// no more messages, flush
+	if len(batch) > 0{
+		err := w.WriteMessages(context.Background(),batch...)
 		if err != nil {
 			fmt.Printf("Failed to publish messages: %s\n", err)
 			os.Exit(1)
 		}
 	}
+	PrintDebug("Finished reading thread")
+	sent <- true
 }
 
 func main() {
 	param_help := flag.Bool("help", false, "prints usage")
 	param_bootstrap := flag.String("bootstrap", "", "zookeeper connect string, typically localhost:9092")
 	param_rate := flag.Int("rate", 10, "msg rate per sec")
-	param_duration := flag.String("duration", "", "Time lapse to send the logs")
+	param_duration := flag.String("duration", "5y", "Time lapse to send the logs")
 	param_topic := flag.String("topic","test-kafka","topic to send msgs to")
 	flag.BoolVar(&dryrun,"dryrun", false,"prints in stdout instead of kafka")
 	flag.BoolVar(&debug,"debug", false, "Debug mode")
@@ -96,20 +128,15 @@ func main() {
 		usage()
 	}
 
-	use_duration := false
 	var duration time.Duration
-
-	if len(*param_duration) > 0 {
-		use_duration = true
-		var err error
-		duration, err = time.ParseDuration(*param_duration)
-		if err != nil {
-			fmt.Println("ERROR: --duration must be a duration format (e.g. 5m, 10s, 5h")
-		}
-		PrintDebug(fmt.Sprintf("Using duration %s", duration))
+	duration, err := time.ParseDuration(*param_duration)
+	if err != nil {
+		fmt.Println("ERROR: --duration must be a duration format (e.g. 5m, 10s, 5h")
 	}
 
-	msgChan = make(chan string,10)
+	msgChan = make(chan kafka.Message,100000)
+	done = make(chan bool)
+	sent = make(chan bool)
 
 	// initiating the connection to kafka
 	if !dryrun {
@@ -125,16 +152,10 @@ func main() {
 		go readMessages()
 	}
 
+	PrintDebug(fmt.Sprintf("Will produce messages at %d msg/sec for %s\n",*param_rate,duration))
+	go ProduceMessages(*param_rate,duration)
 
-	if use_duration {
-		timer := time.NewTimer(duration)
-		PrintDebug(fmt.Sprintf("Will produce messages at %d msg/sec for %s\n",*param_rate,duration))
-		go ProduceMessages(*param_rate)
-		<- timer.C
-	} else {
-		PrintDebug(fmt.Sprintf("Will produce messages at %d msg/minutes\n",*param_rate))
-		ProduceMessages(*param_rate)
-	}
-	//close(msgChan)
+	<- done
+	<- sent
 
 }
